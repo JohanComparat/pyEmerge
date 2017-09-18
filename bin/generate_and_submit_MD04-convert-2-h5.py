4a5115fd2b184d0d009c4dc3fbd58241433389a2
JohanComparat <johan.comparat@gmail.com>
2017-09-18 07:29:04 +0200

    md04

--- bin/generate_and_submit_MD04-convert-2-h5.py
+++ bin/generate_and_submit_MD04-convert-2-h5.py
@@ -7,7 +7,7 @@ import glob
 import numpy as n
 
 aexp, redshift, age_yr, rho_crit, delta_vir = n.loadtxt("/u/joco/data/MD/MD_0.4Gpc/hlists_MD_0.4Gpc.ascii", unpack=True)
-snap_names = n.array([ el.zfill(5)[0]+"."+el.zfill(5)[1:]+"0" for el in (aexp*10**4).astype('int').astype('str') ])
+snap_names = n.array([ el.zfill(6)[0]+"."+el.zfill(6)[1:] for el in (aexp*10**5).astype('int').astype('str') ])
 snap_ids = n.arange(len(aexp))
 #snap_name = snap_names[snap_id]
 
@@ -42,7 +42,7 @@ for snap_id in snap_ids:
     batch_file = os.path.join(batch_dir, "h5_"+snap_names[snap_id]+"_smd.sh")
     f=open( batch_file, 'w')
     f.write(main_text(snap_names[snap_id]))
-    command = "python MD04-convert-2-h5.py " + snap_names[snap_id] +" "+str(aex[psnap_id])+" "+str( redshift[snap_id])+" "+str( age_yr[snap_id])+" "+str( rho_crit[snap_id])+" "+str( delta_vir[snap_id])
+    command = "python MD04-convert-2-h5.py " + snap_names[snap_id] +" "+str(aexp[snap_id])+" "+str( redshift[snap_id])+" "+str( age_yr[snap_id])+" "+str( rho_crit[snap_id])+" "+str( delta_vir[snap_id])
     print(command)
     f.write(command)
     f.close()
