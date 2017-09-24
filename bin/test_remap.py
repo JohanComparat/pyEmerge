7464b7f0c0d93bad887c06f6721bfce498f68714
JohanComparat <johan.comparat@gmail.com>
2017-09-24 21:56:23 +0200

    test

--- bin/test_remap.py
+++ bin/test_remap.py
@@ -9,15 +9,20 @@ from remap import *
 C6 = Cuboid(u1=(5, 3, 1), u2=(1, 1, 0), u3=(0, 1, 0))
 C3 = Cuboid(u1=(2, 1, 1), u2=(1, 1, 0), u3=(0, 1, 0))
 
-def f(x,y,z,L_box=1000.):
-	return n.transpose([C6.Transform(aa,bb,cc) for aa,bb,cc in zip(x/L_box, y/L_box, z/L_box)])*L_box
+def f(DATA):
+	x,y,z = DATA
+	return n.transpose([C6.Transform(aa,bb,cc) for aa,bb,cc in zip(x, y, z)])
+
+def f(aa,bb,cc):
+	return C6.Transform(aa,bb,cc)
     
-    
-x, y, z = n.arange(1000.), n.arange(1000.), n.arange(1000.)
+L_box = 1000.
+dx=0.001
+x, y, z = n.arange(1,1000.,dx)/L_box, n.arange(1,1000.,dx)/L_box, n.arange(1,1000.,dx)/L_box
 
 if __name__ == '__main__':
 	t0=time.time()
-	p = Pool(2)
-	out = p.starmap(f, zip(x, y, z))
+	p = Pool(12)
+	out = p.starmap(f, n.transpose([x, y, z]))
 	print( time.time()-t0 )
 
