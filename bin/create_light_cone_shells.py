ce2833f2c916ddbab7c5f04e8a969c4a01345bf4
JohanComparat <johan.comparat@gmail.com>
2017-11-02 13:32:29 +0100

    shells

--- bin/create_light_cone_shells.py
+++ bin/create_light_cone_shells.py
@@ -7,6 +7,16 @@ L1 L2 L3   u11 u12 u13   u21 u22 u23   u31 u32 u33   (periodicity)
 
 python3 create_light_cone_shells.py 10 MD10 1000
 
+
+python3 create_light_cone_shells.py 10 MD10 1000
+
+import numpy as n
+import os
+for ii in n.arange(50,115,1)[::-1]:
+	comm="python3 create_light_cone_shells.py "+str(ii)+" MD10 1000"	
+	print(comm)
+	os.system(comm)
+
 """
 
 import sys
@@ -34,8 +44,11 @@ if os.path.isdir(h5_lc_dir)==False:
 
 h5_dir = os.path.join(os.environ[env], 'h5' )
 
-input_list = n.array(glob.glob(os.path.join(h5_dir, "hlist_?.?????_emerge.hdf5")))
-input_list.sort()
+input_list_i = n.array(glob.glob(os.path.join(h5_dir, "hlist_?.?????_emerge.hdf5")))
+input_list_i.sort()
+
+# removing snapshots that cannote be remapped ...
+input_list = n.delete(input_list_i, n.argwhere(input_list_i== os.path.join(h5_dir, "hlist_0.71730_emerge.hdf5")))
 
 # creates the redshift list 
 redshifts = []
@@ -51,7 +64,7 @@ Dcom = cosmoMD.comoving_distance(redshifts).value
 Dmax = n.hstack((Dcom[0],(Dcom[1:]+Dcom[:-1])/2.))
 Dmin = n.hstack(((Dcom[1:]+Dcom[:-1])/2., Dcom[-1]))
 
-def copylc_data_data(ii):
+def copylc_data(ii, option=False):
 	"""
 	Creates the selection array to obtain the shell in a snapshot to be added in the light cone
 	
@@ -62,99 +75,89 @@ def copylc_data_data(ii):
 	print(file_1, "==>>", file_out)
 	f1 = h5py.File(file_1,  "r")
 	print( "n halos=",f1['/halo_properties/'].attrs['N_halos'])
-	x,y,z=f1[positions_group_name + '/xyx_Lbox'].value.T*L_box
+	if option:
+		XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+	else:
+		x,y,z=f1[positions_group_name + '/xyx_Lbox'].value.T*L_box
+
 	distance = ((x-x_obs)**2.+(y-y_obs)**2.+(z-z_obs)**2.)**0.5
 	selection = (distance>=Dmin[ii])&(distance<Dmax[ii])
 	print( len(distance[selection])," halos in shell ", Dmin[ii], "<d comoving<",Dmax[ii])
 	
-	f = h5py.File(file_out, "a")
-	f.attrs['file_name'] = os.path.basename(file_out)
-	f.attrs['HDF5_Version']     = h5py.version.hdf5_version
-	f.attrs['h5py_version']     = h5py.version.version
+	if len(distance[selection])>1:
+		f = h5py.File(file_out, "a")
+		f.attrs['file_name'] = os.path.basename(file_out)
+		f.attrs['HDF5_Version']     = h5py.version.hdf5_version
+		f.attrs['h5py_version']     = h5py.version.version
 
-	halo_data = f.create_group('halo_position')
+		halo_data = f.create_group('halo_position')
 
-	ds = halo_data.create_dataset('x', data = x[selection] )
-	ds.attrs['units'] = 'Mpc/h'
-	ds.attrs['long_name'] = 'x' 
-	ds = halo_data.create_dataset('y', data = y[selection] )
-	ds.attrs['units'] = 'Mpc/h'
-	ds.attrs['long_name'] = 'y' 
-	ds = halo_data.create_dataset('z', data = z[selection] )
-	ds.attrs['units'] = 'Mpc/h'
-	ds.attrs['long_name'] = 'z' 
+		ds = halo_data.create_dataset('x', data = x[selection] )
+		ds.attrs['units'] = 'Mpc/h'
+		ds.attrs['long_name'] = 'x' 
+		ds = halo_data.create_dataset('y', data = y[selection] )
+		ds.attrs['units'] = 'Mpc/h'
+		ds.attrs['long_name'] = 'y' 
+		ds = halo_data.create_dataset('z', data = z[selection] )
+		ds.attrs['units'] = 'Mpc/h'
+		ds.attrs['long_name'] = 'z' 
 
-	ds = halo_data.create_dataset('vx', data = f1['/halo_position/vx'].value[selection] )
-	ds.attrs['units'] = 'km/s'
-	ds.attrs['long_name'] = 'vx' 
-	ds = halo_data.create_dataset('vy', data = f1['/halo_position/vy'].value[selection] )
-	ds.attrs['units'] = 'km/s'
-	ds.attrs['long_name'] = 'vy' 
-	ds = halo_data.create_dataset('vz', data = f1['/halo_position/vz'].value[selection] )
-	ds.attrs['units'] = 'km/s'
-	ds.attrs['long_name'] = 'vz' 
+		ds = halo_data.create_dataset('vx', data = f1['/halo_position/vx'].value[selection] )
+		ds.attrs['units'] = 'km/s'
+		ds.attrs['long_name'] = 'vx' 
+		ds = halo_data.create_dataset('vy', data = f1['/halo_position/vy'].value[selection] )
+		ds.attrs['units'] = 'km/s'
+		ds.attrs['long_name'] = 'vy' 
+		ds = halo_data.create_dataset('vz', data = f1['/halo_position/vz'].value[selection] )
+		ds.attrs['units'] = 'km/s'
+		ds.attrs['long_name'] = 'vz' 
 
-	halo_data = f.create_group('halo_properties')
-	
-	ds = halo_data.create_dataset('id'                   , data = f1['/halo_properties/id'].value[selection] )
-	ds.attrs['units'] = '-'
-	ds.attrs['long_name'] = 'halo identifier' 
+		halo_data = f.create_group('halo_properties')
+		
+		ds = halo_data.create_dataset('id', data = f1['/halo_properties/id'].value[selection] )
+		ds.attrs['units'] = '-'
+		ds.attrs['long_name'] = 'halo identifier' 
 
-	ds = halo_data.create_dataset('pid'                  , data = f1['/halo_properties/pid'].value[selection]                      )
-	ds.attrs['units'] = '-'
-	ds.attrs['long_name'] = 'parent identifier, -1 if distinct halo' 
+		ds = halo_data.create_dataset('pid', data = f1['/halo_properties/pid'].value[selection] )
+		ds.attrs['units'] = '-'
+		ds.attrs['long_name'] = 'parent identifier, -1 if distinct halo' 
 
-	ds = halo_data.create_dataset('mvir'                 , data = f1['/halo_properties/mvir'].value[selection]                     )
-	ds.attrs['units'] = r'$h^{-1} M_\odot$'
-	ds.attrs['long_name'] = r'$M_{vir}$' 
+		ds = halo_data.create_dataset('mvir', data = f1['/halo_properties/mvir'].value[selection] )
+		ds.attrs['units'] = r'$h^{-1} M_\odot$'
+		ds.attrs['long_name'] = r'$M_{vir}$' 
 
-	ds = halo_data.create_dataset('rvir'                 , data = f1['/halo_properties/rvir'].value[selection]                     )
-	ds.attrs['units'] = r'$h^{-1} kpc$'
-	ds.attrs['long_name'] = r'$r_{vir}$' 
+		ds = halo_data.create_dataset('rvir' data = f1['/halo_properties/rvir'].value[selection] )
+		ds.attrs['units'] = r'$h^{-1} kpc$'
+		ds.attrs['long_name'] = r'$r_{vir}$' 
 
-	ds = halo_data.create_dataset('rs'                   , data = f1['/halo_properties/rs'].value[selection]                       )
-	ds.attrs['units'] = r'$h^{-1} kpc$'
-	ds.attrs['long_name'] = r'$r_{s}$' 
+		ds = halo_data.create_dataset('rs', data = f1['/halo_properties/rs'].value[selection] )
+		ds.attrs['units'] = r'$h^{-1} kpc$'
+		ds.attrs['long_name'] = r'$r_{s}$' 
 
-	ds = halo_data.create_dataset('Vmax'                , data = f1['/halo_properties/Vmax'].value[selection]                    )
-	ds.attrs['units'] = 'km/s'
-	ds.attrs['long_name'] = r'$V_{max}$' 
+		ds = halo_data.create_dataset('Vmax' , data = f1['/halo_properties/Vmax'].value[selection])
+		ds.attrs['units'] = 'km/s'
+		ds.attrs['long_name'] = r'$V_{max}$' 
 
-	ds = halo_data.create_dataset('Mpeak'                , data = f1['/halo_properties/Mpeak'].value[selection]                    )
-	ds.attrs['units'] = r'$h^{-1} M_\odot$'
-	ds.attrs['long_name'] = r'$M_{peak}$' 
+		ds = halo_data.create_dataset('Mpeak' , data = f1['/halo_properties/Mpeak'].value[selection])
+		ds.attrs['units'] = r'$h^{-1} M_\odot$'
+		ds.attrs['long_name'] = r'$M_{peak}$' 
 
-	emerge_data = f.create_group('emerge_data')
-	
-	ds = emerge_data.create_dataset('stellar_mass'                 , data = f1['/emerge_data/stellar_mass'].value[selection]                     )
-	ds.attrs['units'] = r'$ M_\odot$'
-	ds.attrs['long_name'] = 'stellar mass' 
+		emerge_data = f.create_group('emerge_data')
+		
+		ds = emerge_data.create_dataset('stellar_mass', data = f1['/emerge_data/stellar_mass'].value[selection])
+		ds.attrs['units'] = r'$ M_\odot$'
+		ds.attrs['long_name'] = 'stellar mass' 
 
-	ds = emerge_data.create_dataset('m_icm'                 , data = f1['/emerge_data/m_icm'].value[selection]                     )
-	ds.attrs['units'] = r'$ M_\odot$'
-	ds.attrs['long_name'] = 'ICM mass' 
+		ds = emerge_data.create_dataset('m_icm', data = f1['/emerge_data/m_icm'].value[selection] )
+		ds.attrs['units'] = r'$ M_\odot$'
+		ds.attrs['long_name'] = 'ICM mass' 
 
-	ds = emerge_data.create_dataset('mvir'                 , data = f1['/emerge_data/star_formation_rate'].value[selection]                     )
-	ds.attrs['units'] = r'$ M_\odot /yr $'
-	ds.attrs['long_name'] = 'star formation rate' 
+		ds = emerge_data.create_dataset('star_formation_rate', data = f1['/emerge_data/star_formation_rate'].value[selection]                     )
+		ds.attrs['units'] = r'$ M_\odot /yr $'
+		ds.attrs['long_name'] = 'star formation rate' 
 
-	f.close()
+		f.close()
 	f1.close()
 
 
-copylc_data_data(ii)
-#ii=-10
-#array_h5_names= n.array([
-	#'/halo_position/vx', 
-	#'/halo_position/vy', 
-	#'/halo_position/vz', 
-	#'/halo_properties/Mpeak', 
-	#'/halo_properties/Vmax', 
-	#'/halo_properties/id', 
-	#'/halo_properties/mvir', 
-	#'/halo_properties/pid', 
-	#'/halo_properties/rvir', 
-	#'/halo_properties/rs', 
-	#'/emerge_data/stellar_mass', 
-	#'/emerge_data/star_formation_rate'
-	#])
+copylc_data(ii)
