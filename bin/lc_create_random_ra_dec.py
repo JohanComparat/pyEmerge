6899440656fd18631b03a4193c7ef1cbafe7531a
JohanComparat <johan.comparat@gmail.com>
2017-11-08 17:28:57 +0100

    lognlogs

--- bin/lc_create_random_ra_dec.py
+++ bin/lc_create_random_ra_dec.py
@@ -3,7 +3,7 @@ import numpy as n
 topdir = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/clustering_catalogs_remaped_position_L3/'
 
 
-size = 30000000
+size = 300000000
 NN = 3 # or any positive integer
 x = n.random.normal(size=(size, NN)) 
 x /= n.linalg.norm(x, axis=1)[:, n.newaxis]
