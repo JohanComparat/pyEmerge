b8cf72dfc283dbe4d8fdd344ec21502fa23ffec1
JohanComparat <johan.comparat@gmail.com>
2017-11-08 17:19:58 +0100

    lognlogs

---
+++ bin/lc_create_random_ra_dec.py
@@ -0,0 +1,18 @@
+import numpy as n
+
+topdir = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/clustering_catalogs_remaped_position_L3/'
+
+
+size = 30000000
+NN = 3 # or any positive integer
+x = n.random.normal(size=(size, NN)) 
+x /= n.linalg.norm(x, axis=1)[:, numpy.newaxis]
+
+theta = n.arctan(x.T[1]/x.T[2])*180/n.pi
+phi = n.arctan((x.T[0]**2+x.T[1]**2)**0.5/x.T[2])*180/n.pi
+
+sel = (theta<6.7529257176359)&(theta>-6.7529257176359)&(phi>-8.269819492449505)&(phi<8.269819492449505)
+
+n.savetxt(topdir+'random-ra-dec.txt', n.transpose([theta[sel], phi[sel]]))
+
+
