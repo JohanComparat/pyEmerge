8d82e898b1a046a18e1f52b692cd2f174c6177a9
JohanComparat <johan.comparat@gmail.com>
2017-12-03 22:13:26 +0100

    cluster h5 files

--- python/EmergeMultiDark.py
+++ python/EmergeMultiDark.py
@@ -406,3 +406,292 @@ class MultiDarkSimulation :
 
 		f.close()
 
+	def convert_to_h5_format_cluster(self, snap_name, path_2_h5_file, aexp, redshift):
+
+		timestr = time.strftime("%Y%m%d-%H%M%S")
+		
+		#path_2_snap = "/u/joco/data/MD/MD_1.0Gpc/emerge/hlist_" + snap_name + ".data"
+		#path_2_h5_file = "/u/joco/data/MD/MD_1.0Gpc/h5/hlist_"  + snap_name + "_emerge.hdf5"
+		if os.path.isfile(path_2_h5_file):
+			os.system("rm "+path_2_h5_file)
+
+		dtype1 = n.dtype([
+		( 'scale'                             , 'float64' )
+		,('id'                                , 'int64' )
+		,('desc_scale'                        , 'float64' )
+		,('desc_id'                           , 'int64' )
+		,('num_prog'                          , 'float64' )
+		,('pid'                               , 'int64' )
+		,('upid'                              , 'int64' )
+		,('desc_pid'                          , 'int64' )
+		,('phantom'                           , 'float64' )
+		,('sam_mvir'                          , 'float64' )
+		,('mvir'                              , 'float64' )
+		,('rvir'                              , 'float64' )
+		,('rs'                                , 'float64' )
+		,('vrms'                              , 'float64' )
+		,('mmp?'                              , 'float64' )
+		,('scale_of_last_MM'                  , 'float64' )
+		,('vmax'                              , 'float64' )
+		,('x'                                 , 'float64' )
+		,('y'                                 , 'float64' )
+		,('z'                                 , 'float64' )
+		,('vx'                                , 'float64' )
+		,('vy'                                , 'float64' )
+		,('vz'                                , 'float64' )
+		,('Jx'                                , 'float64' )
+		,('Jy'                                , 'float64' )
+		,('Jz'                                , 'float64' )
+		,('Spin'                              , 'float64' )
+		,('Breadth_first_ID'                  , 'int64' )
+		,('Depth_first_ID'                    , 'int64' )
+		,('Tree_root_ID'                      , 'int64' )
+		,('Orig_halo_ID'                      , 'int64' )
+		,('Snap_num'                          , 'int64' )
+		,('Next_coprogenitor_depthfirst_ID'   , 'int64' )
+		,('Last_progenitor_depthfirst_ID'     , 'int64' )
+		,('Last_mainleaf_depthfirst_ID'       , 'int64' )
+		,('Tidal_Force'                       , 'float64' )
+		,('Tidal_ID'                          , 'int64' )
+		,('Rs_Klypin'                         , 'float64' )
+		,('Mmvir_all'                         , 'float64' )
+		,('M200b'                             , 'float64' )
+		,('M200c'                             , 'float64' )
+		,('M500c'                             , 'float64' )
+		,('M2500c'                            , 'float64' )
+		,('Xoff'                              , 'float64' )
+		,('Voff'                              , 'float64' )
+		,('Spin_Bullock'                      , 'float64' )
+		,('b_to_a'                            , 'float64' )
+		,('c_to_a'                            , 'float64' )
+		,('Ax'                                , 'float64' )
+		,('Ay'                                , 'float64' )
+		,('Az'                                , 'float64' )
+		,('b_to_a_500c'                       , 'float64' )
+		,('c_to_a_500c'                       , 'float64' )
+		,('Ax_500c'                           , 'float64' )
+		,('Ay_500c'                           , 'float64' )
+		,('Az_500c'                           , 'float64' )
+		,('TU'                                , 'float64' )
+		,('M_pe_Behroozi'                     , 'float64' )
+		,('M_pe_Diemer'                       , 'float64' )
+		,('Macc'                              , 'float64' )
+		,('Mpeak'                             , 'float64' )
+		,('Vacc'                              , 'float64' )
+		,('Vpeak'                             , 'float64' )
+		,('Halfmass_Scale'                    , 'float64' )
+		,('Acc_Rate_Inst'                     , 'float64' )
+		,('Acc_Rate_100Myr'                   , 'float64' )
+		,('Acc_Rate_1Tdyn'                    , 'float64' )
+		,('Acc_Rate_2Tdyn'                    , 'float64' )
+		,('Acc_Rate_Mpeak'                    , 'float64' )
+		,('Mpeak_Scale'                       , 'float64' )
+		,('Acc_Scale'                         , 'float64' )
+		,('First_Acc_Scale'                   , 'float64' )
+		,('First_Acc_Mvir'                    , 'float64' )
+		,('First_Acc_Vmax'                    , 'float64' )
+		,('VmaxAtMpeak'                       , 'float64' )
+		,('Tidal_Force_Tdyn'                  , 'float64' )
+		,('logVmaxVmaxmaxTdynTmpeak'          , 'float64' )
+		,('Time_to_future_merger'             , 'float64' )
+		,('Future_merger_MMP_ID'              , 'int64' )
+		])
+		
+		scale                             ,
+		id                                ,
+		desc_scale                        ,
+		desc_id                           ,
+		num_prog                          ,
+		pid                               ,
+		upid                              ,
+		desc_pid                          ,
+		phantom                           ,
+		sam_mvir                          ,
+		mvir                              ,
+		rvir                              ,
+		rs                                ,
+		vrms                              ,
+		mmp?                              ,
+		scale_of_last_MM                  ,
+		vmax                              ,
+		x                                 ,
+		y                                 ,
+		z                                 ,
+		vx                                ,
+		vy                                ,
+		vz                                ,
+		Jx                                ,
+		Jy                                ,
+		Jz                                ,
+		Spin                              ,
+		Breadth_first_ID                  ,
+		Depth_first_ID                    ,
+		Tree_root_ID                      ,
+		Orig_halo_ID                      ,
+		Snap_num                          ,
+		Next_coprogenitor_depthfirst_ID   ,
+		Last_progenitor_depthfirst_ID     ,
+		Last_mainleaf_depthfirst_ID       ,
+		Tidal_Force                       ,
+		Tidal_ID                          ,
+		Rs_Klypin                         ,
+		Mmvir_all                         ,
+		M200b                             ,
+		M200c                             ,
+		M500c                             ,
+		M2500c                            ,
+		Xoff                              ,
+		Voff                              ,
+		Spin_Bullock                      ,
+		b_to_a                            ,
+		c_to_a                            ,
+		Ax                                ,
+		Ay                                ,
+		Az                                ,
+		b_to_a_500c                       ,
+		c_to_a_500c                       ,
+		Ax_500c                           ,
+		Ay_500c                           ,
+		Az_500c                           ,
+		TU                                ,
+		M_pe_Behroozi                     ,
+		M_pe_Diemer                       ,
+		Macc                              ,
+		Mpeak                             ,
+		Vacc                              ,
+		Vpeak                             ,
+		Halfmass_Scale                    ,
+		Acc_Rate_Inst                     ,
+		Acc_Rate_100Myr                   ,
+		Acc_Rate_1Tdyn                    ,
+		Acc_Rate_2Tdyn                    ,
+		Acc_Rate_Mpeak                    ,
+		Mpeak_Scale                       ,
+		Acc_Scale                         ,
+		First_Acc_Scale                   ,
+		First_Acc_Mvir                    ,
+		First_Acc_Vmax                    ,
+		VmaxAtMpeak                       ,
+		Tidal_Force_Tdyn                  ,
+		logVmaxVmaxmaxTdynTmpeak          ,
+		Time_to_future_merger             ,
+		Future_merger_MMP_ID      =  n.loadtxt(path_2_snap, unpack=True, dtype = dtype1)
+		
+		N_halo = len(id)
+
+		# create the h5 container to host the data
+
+		f = h5py.File(path_2_h5_file, "a")
+		f.attrs['file_name'] = os.path.basename(path_2_h5_file)
+		f.attrs['file_time'] = timestr
+		f.attrs['creator']   = 'JC'
+		f.attrs['HDF5_Version']     = h5py.version.hdf5_version
+		f.attrs['h5py_version']     = h5py.version.version
+
+		f.attrs['aexp']      = float(aexp)#[snap_id]
+		f.attrs['redshift']  = float(redshift)#[snap_id]
+		#f.attrs['age_yr']    = float(age_yr)#[snap_id] 
+		#f.attrs['rho_crit']  = float(rho_crit)#[snap_id] 
+		#f.attrs['delta_vir'] = float(delta_vir)#[snap_id]
+
+		halo_data = f.create_group('halo_position')
+		
+		ds = halo_data.create_dataset('x', data = x )
+		ds.attrs['units'] = 'Mpc/h'
+		ds.attrs['long_name'] = 'x' 
+		ds = halo_data.create_dataset('y', data = y )
+		ds.attrs['units'] = 'Mpc/h'
+		ds.attrs['long_name'] = 'y' 
+		ds = halo_data.create_dataset('z', data = z )
+		ds.attrs['units'] = 'Mpc/h'
+		ds.attrs['long_name'] = 'z' 
+
+		ds = halo_data.create_dataset('vx', data = vx )
+		ds.attrs['units'] = 'km/s'
+		ds.attrs['long_name'] = 'vx' 
+		ds = halo_data.create_dataset('vy', data = vy )
+		ds.attrs['units'] = 'km/s'
+		ds.attrs['long_name'] = 'vy' 
+		ds = halo_data.create_dataset('vz', data = vz )
+		ds.attrs['units'] = 'km/s'
+		ds.attrs['long_name'] = 'vz' 
+		
+		halo_data = f.create_group('halo_properties')
+		halo_data.attrs['N_halos'] =  N_halo
+
+		ds = halo_data.create_dataset('scale'                              , data=scale                            )
+		ds = halo_data.create_dataset('id'                                 , data=id                               )
+		ds = halo_data.create_dataset('desc_scale'                         , data=desc_scale                       )
+		ds = halo_data.create_dataset('desc_id'                            , data=desc_id                          )
+		ds = halo_data.create_dataset('num_prog'                           , data=num_prog                         )
+		ds = halo_data.create_dataset('pid'                                , data=pid                              )
+		ds = halo_data.create_dataset('upid'                               , data=upid                             )
+		ds = halo_data.create_dataset('desc_pid'                           , data=desc_pid                         )
+		ds = halo_data.create_dataset('phantom'                            , data=phantom                          )
+		ds = halo_data.create_dataset('sam_mvir'                           , data=sam_mvir                         )
+		ds = halo_data.create_dataset('mvir'                               , data=mvir                             )
+		ds = halo_data.create_dataset('rvir'                               , data=rvir                             )
+		ds = halo_data.create_dataset('rs'                                 , data=rs                               )
+		ds = halo_data.create_dataset('vrms'                               , data=vrms                             )
+		ds = halo_data.create_dataset('mmp?'                               , data=mmp?                             )
+		ds = halo_data.create_dataset('scale_of_last_MM'                   , data=scale_of_last_MM                 )
+		ds = halo_data.create_dataset('vmax'                               , data=vmax                             )
+		ds = halo_data.create_dataset('Jx'                                 , data=Jx                               )
+		ds = halo_data.create_dataset('Jy'                                 , data=Jy                               )
+		ds = halo_data.create_dataset('Jz'                                 , data=Jz                               )
+		ds = halo_data.create_dataset('Spin'                               , data=Spin                             )
+		ds = halo_data.create_dataset('Breadth_first_ID'                   , data=Breadth_first_ID                 )
+		ds = halo_data.create_dataset('Depth_first_ID'                     , data=Depth_first_ID                   )
+		ds = halo_data.create_dataset('Tree_root_ID'                       , data=Tree_root_ID                     )
+		ds = halo_data.create_dataset('Orig_halo_ID'                       , data=Orig_halo_ID                     )
+		ds = halo_data.create_dataset('Snap_num'                           , data=Snap_num                         )
+		ds = halo_data.create_dataset('Next_coprogenitor_depthfirst_ID'    , data=Next_coprogenitor_depthfirst_ID  )
+		ds = halo_data.create_dataset('Last_progenitor_depthfirst_ID'      , data=Last_progenitor_depthfirst_ID    )
+		ds = halo_data.create_dataset('Last_mainleaf_depthfirst_ID'        , data=Last_mainleaf_depthfirst_ID      )
+		ds = halo_data.create_dataset('Tidal_Force'                        , data=Tidal_Force                      )
+		ds = halo_data.create_dataset('Tidal_ID'                           , data=Tidal_ID                         )
+		ds = halo_data.create_dataset('Rs_Klypin'                          , data=Rs_Klypin                        )
+		ds = halo_data.create_dataset('Mmvir_all'                          , data=Mmvir_all                        )
+		ds = halo_data.create_dataset('M200b'                              , data=M200b                            )
+		ds = halo_data.create_dataset('M200c'                              , data=M200c                            )
+		ds = halo_data.create_dataset('M500c'                              , data=M500c                            )
+		ds = halo_data.create_dataset('M2500c'                             , data=M2500c                           )
+		ds = halo_data.create_dataset('Xoff'                               , data=Xoff                             )
+		ds = halo_data.create_dataset('Voff'                               , data=Voff                             )
+		ds = halo_data.create_dataset('Spin_Bullock'                       , data=Spin_Bullock                     )
+		ds = halo_data.create_dataset('b_to_a'                             , data=b_to_a                           )
+		ds = halo_data.create_dataset('c_to_a'                             , data=c_to_a                           )
+		ds = halo_data.create_dataset('Ax'                                 , data=Ax                               )
+		ds = halo_data.create_dataset('Ay'                                 , data=Ay                               )
+		ds = halo_data.create_dataset('Az'                                 , data=Az                               )
+		ds = halo_data.create_dataset('b_to_a_500c'                        , data=b_to_a_500c                      )
+		ds = halo_data.create_dataset('c_to_a_500c'                        , data=c_to_a_500c                      )
+		ds = halo_data.create_dataset('Ax_500c'                            , data=Ax_500c                          )
+		ds = halo_data.create_dataset('Ay_500c'                            , data=Ay_500c                          )
+		ds = halo_data.create_dataset('Az_500c'                            , data=Az_500c                          )
+		ds = halo_data.create_dataset('TU'                                 , data=TU                               )
+		ds = halo_data.create_dataset('M_pe_Behroozi'                      , data=M_pe_Behroozi                    )
+		ds = halo_data.create_dataset('M_pe_Diemer'                        , data=M_pe_Diemer                      )
+		ds = halo_data.create_dataset('Macc'                               , data=Macc                             )
+		ds = halo_data.create_dataset('Mpeak'                              , data=Mpeak                            )
+		ds = halo_data.create_dataset('Vacc'                               , data=Vacc                             )
+		ds = halo_data.create_dataset('Vpeak'                              , data=Vpeak                            )
+		ds = halo_data.create_dataset('Halfmass_Scale'                     , data=Halfmass_Scale                   )
+		ds = halo_data.create_dataset('Acc_Rate_Inst'                      , data=Acc_Rate_Inst                    )
+		ds = halo_data.create_dataset('Acc_Rate_100Myr'                    , data=Acc_Rate_100Myr                  )
+		ds = halo_data.create_dataset('Acc_Rate_1Tdyn'                     , data=Acc_Rate_1Tdyn                   )
+		ds = halo_data.create_dataset('Acc_Rate_2Tdyn'                     , data=Acc_Rate_2Tdyn                   )
+		ds = halo_data.create_dataset('Acc_Rate_Mpeak'                     , data=Acc_Rate_Mpeak                   )
+		ds = halo_data.create_dataset('Mpeak_Scale'                        , data=Mpeak_Scale                      )
+		ds = halo_data.create_dataset('Acc_Scale'                          , data=Acc_Scale                        )
+		ds = halo_data.create_dataset('First_Acc_Scale'                    , data=First_Acc_Scale                  )
+		ds = halo_data.create_dataset('First_Acc_Mvir'                     , data=First_Acc_Mvir                   )
+		ds = halo_data.create_dataset('First_Acc_Vmax'                     , data=First_Acc_Vmax                   )
+		ds = halo_data.create_dataset('VmaxAtMpeak'                        , data=VmaxAtMpeak                      )
+		ds = halo_data.create_dataset('Tidal_Force_Tdyn'                   , data=Tidal_Force_Tdyn                 )
+		ds = halo_data.create_dataset('logVmaxVmaxmaxTdynTmpeak'           , data=logVmaxVmaxmaxTdynTmpeak         )
+		ds = halo_data.create_dataset('Time_to_future_merger'              , data=Time_to_future_merger            )
+		ds = halo_data.create_dataset('Future_merger_MMP_ID'               , data=Future_merger_MMP_ID             )
+
+		f.close()
+	
