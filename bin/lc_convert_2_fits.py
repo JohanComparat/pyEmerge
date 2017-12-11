e29b62abb581950f8c0ed1bfab447b3679f1c708
JohanComparat <johan.comparat@gmail.com>
2017-12-11 21:28:21 +0100

    convert 2 fits

--- bin/lc_convert_2_fits.py
+++ bin/lc_convert_2_fits.py
@@ -1,66 +1,3 @@
-"""
-
-+ + + + + + + HEADER + + + + + + + + +
-file_name lc_remaped_position_L3_.hdf5
-HDF5_Version 1.8.18
-h5py_version 2.7.1
-
-
-+ + + + + + + DATA   + + + + + + + + + +
-========================================
-agn_properties <HDF5 group "/agn_properties" (2 members)>
-- - - - - - - - - - - - - - - - - - - - 
-agn_activity <HDF5 dataset "agn_activity": shape (23191107,), type "<f8">
-log_lambda_sar <HDF5 dataset "log_lambda_sar": shape (23191107,), type "<f8">
-========================================
-emerge_data <HDF5 group "/emerge_data" (3 members)>
-- - - - - - - - - - - - - - - - - - - - 
-dMdt <HDF5 dataset "dMdt": shape (23191107,), type "<f8">
-mvir_dot <HDF5 dataset "mvir_dot": shape (23191107,), type "<f8">
-rvir_dot <HDF5 dataset "rvir_dot": shape (23191107,), type "<f8">
-========================================
-halo_position <HDF5 group "/halo_position" (7 members)>
-- - - - - - - - - - - - - - - - - - - - 
-vx <HDF5 dataset "vx": shape (23191107,), type "<f8">
-vy <HDF5 dataset "vy": shape (23191107,), type "<f8">
-vz <HDF5 dataset "vz": shape (23191107,), type "<f8">
-x <HDF5 dataset "x": shape (23191107,), type "<f8">
-y <HDF5 dataset "y": shape (23191107,), type "<f8">
-z <HDF5 dataset "z": shape (23191107,), type "<f8">
-z_snap <HDF5 dataset "z_snap": shape (23191107,), type "<f8">
-========================================
-halo_properties <HDF5 group "/halo_properties" (7 members)>
-- - - - - - - - - - - - - - - - - - - - 
-Mpeak <HDF5 dataset "Mpeak": shape (23191107,), type "<f8">
-Vmax <HDF5 dataset "Vmax": shape (23191107,), type "<f8">
-id <HDF5 dataset "id": shape (23191107,), type "<f8">
-mvir <HDF5 dataset "mvir": shape (23191107,), type "<f8">
-pid <HDF5 dataset "pid": shape (23191107,), type "<f8">
-rs <HDF5 dataset "rs": shape (23191107,), type "<f8">
-rvir <HDF5 dataset "rvir": shape (23191107,), type "<f8">
-========================================
-moster_2013_data <HDF5 group "/moster_2013_data" (1 members)>
-- - - - - - - - - - - - - - - - - - - - 
-stellar_mass <HDF5 dataset "stellar_mass": shape (23191107,), type "<f8">
-========================================
-sky_position <HDF5 group "/sky_position" (5 members)>
-- - - - - - - - - - - - - - - - - - - - 
-DEC <HDF5 dataset "DEC": shape (23191107,), type "<f8">
-RA <HDF5 dataset "RA": shape (23191107,), type "<f8">
-redshift_R <HDF5 dataset "redshift_R": shape (23191107,), type "<f8">
-redshift_S <HDF5 dataset "redshift_S": shape (23191107,), type "<f8">
-selection <HDF5 dataset "selection": shape (23191107,), type "|b1">
-"""
-"""
-Convert to observed fluxes
-
-intrinsic extinction. Thin / thick obscuration
-
-Follows Buchner et al. 2016
-
-"""
-
-
 import h5py    # HDF5 support
 import os
 import glob
@@ -76,7 +13,7 @@ cosmoMD = FlatLambdaCDM(H0=67.77*u.km/u.s/u.Mpc, Om0=0.307115, Ob0=0.048206)
 def write_fits_lc(path_to_lc, out_filename, z_min, z_max, dec_max, ra_max):
   f = h5py.File(path_to_lc, 'r')
 
-  is_gal = (f['/sky_position/selection'].value)&(f['/sky_position/redshift_R'].value<z_max)&(f['/agn_properties/agn_activity'].value==1)
+  is_gal = (f['/sky_position/selection'].value)&(f['/sky_position/redshift_R'].value<z_max)#&(f['/agn_properties/agn_activity'].value==1)
 
   hdu_cols  = fits.ColDefs([
   #fits.Column(name='is_agn',format='K',array= (f['/agn_properties/agn_activity'].value[is_gal]).astype('int'), unit='=1 is AGN' ) 
@@ -94,6 +31,11 @@ def write_fits_lc(path_to_lc, out_filename, z_min, z_max, dec_max, ra_max):
   ,fits.Column(name='DEC',format='D',    array= f['/sky_position/DEC'].value[is_gal], unit='DEC/[deg]'   ) 
   ,fits.Column(name='redshift_R',format='D',    array= f['/sky_position/redshift_R'].value[is_gal], unit='real space redshift'  ) 
   ,fits.Column(name='redshift_S',format='D',    array= f['/sky_position/redshift_S'].value[is_gal], unit='redshift space redshift'  ) 
+  ,fits.Column(name='is_BG_lz',format='L',    array=f['/cosmo_4most/is_BG_lz'].value[is_gal]) 
+  ,fits.Column(name='is_BG_hz',format='L',    array=f['/cosmo_4most/is_BG_hz'].value[is_gal]) 
+  ,fits.Column(name='is_ELG',format='L',    array=f['/cosmo_4most/is_ELG'].value[is_gal]) 
+  ,fits.Column(name='is_QSO',format='L',    array=f['/cosmo_4most/is_QSO'].value[is_gal]) 
+  #,fits.Column(name='is_Lya',format='L',    array=f['/cosmo_4most/is_Lya'].value[is_gal]) 
   ])
 
   f.close()
@@ -119,32 +61,32 @@ def write_fits_lc(path_to_lc, out_filename, z_min, z_max, dec_max, ra_max):
 
 
 
-path_to_lc = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_remaped_position_L3.hdf5'
-out_filename = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_remaped_position_L3.fits'
+path_to_lc = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_L3.hdf5'
+out_filename = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_L3.fits'
 z_min = 0.
 z_max = 1.08
 dec_max = 8.269819492449505
 ra_max = 6.7529257176359
 write_fits_lc(path_to_lc, out_filename, z_min, z_max, dec_max, ra_max)
 
-path_to_lc = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_remaped_position_L3_z1.hdf5'
-out_filename = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_remaped_position_L3_z1.fits'
+path_to_lc = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_L3_z1.hdf5'
+out_filename = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_L3_z1.fits'
 z_min = 1.08
 z_max = 3.0
 dec_max = 4.134909746242654
 ra_max = 3.3764628588325674
 write_fits_lc(path_to_lc, out_filename, z_min, z_max, dec_max, ra_max)
 
-path_to_lc = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_remaped_position_L6.hdf5'
-out_filename = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_remaped_position_L6.fits'
+path_to_lc = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_L6.hdf5'
+out_filename = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_L6.fits'
 z_min = 0.
 z_max = 3.0
 dec_max = 2.0047373031569915
 ra_max = 1.9766516114702513
 write_fits_lc(path_to_lc, out_filename, z_min, z_max, dec_max, ra_max)
 
-path_to_lc = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_remaped_position_L15.hdf5'
-out_filename = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_remaped_position_L15.fits'
+path_to_lc = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_L15.hdf5'
+out_filename = '/data17s/darksim/MD/MD_1.0Gpc/h5_lc/lc_L15.fits'
 z_min = 0.
 z_max = 0.54
 dec_max = 20.257311381848154
